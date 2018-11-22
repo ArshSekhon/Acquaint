cdcab0aca6bf5de127beda3fae69dc905ac7d4a9
aman <aman.s.s@outlook.com>
2018-11-21 23:53:55 -0800

    more templates

--- flask_dir/src/routes.py
+++ flask_dir/src/routes.py
@@ -30,11 +30,12 @@ def index():
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v2/userinfo")
     assert resp.ok, resp.text
-    return "You are {email} on Google".format(email=resp.json()["email"])
+    #return "You are {email} on Google".format(email=resp.json()["email"])
+    return render_template('index.html')
 
 @app.route("/login")
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
